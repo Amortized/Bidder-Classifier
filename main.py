53d039ef6f5400c92ffa09dde369e1bf18217a13
Amortized <rahulmanghwani@gmail.com>
2015-05-31 03:13:28 -0700

    adding more features

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ def read(train_file, test_file, bids_file):
 
 	return train, test, train_bids, test_bids;
 
+def getStats(grouped):
+	return len(grouped.keys()), \
+		   np.min(grouped.values), \
+		   np.max(grouped.values), \
+		   np.mean(grouped.values), \
+		   np.median(grouped.values), \
+		   np.std(grouped.values), \
+		   len(grouped.values[grouped.values==1]), \
+		   len(grouped.values[grouped.values==1]) / float(len(grouped.values));
+
+
 
 def computeFeatures(data_bids):
 	#Convert into a matrix
@@ -25,30 +36,82 @@ def computeFeatures(data_bids):
 
 	for name,group in data_bids:
 		auctions  			    = group['auction'].value_counts(ascending=False,sort=True,dropna=True);
+		devices  			    = group['device'].value_counts(ascending=False,sort=True,dropna=True);
+		merchandise  			= group['merchandise'].value_counts(ascending=False,sort=True,dropna=True);
+		countries  			    = group['country'].value_counts(ascending=False,sort=True,dropna=True);
+		ips		  			    = group['ip'].value_counts(ascending=False,sort=True,dropna=True);
+		urls  					= group['url'].value_counts(ascending=False,sort=True,dropna=True);
+
+		auction_devices         = group[['auction','device']].groupby('auction');
+		auction_countries       = group[['auction','country']].groupby('auction');
+		auction_ips             = group[['auction','ip']].groupby('auction');
 
 		#Auction Features
-		no_of_distinct_auctions      = len(auctions.keys());
-		min_bids_in_an_auction       = np.min(auctions.values);
-		max_bids_in_an_auction       = np.max(auctions.values);
-		avg_bids_in_an_auction       = np.mean(auctions.values);
-		median_bids_in_an_auction    = np.median(auctions.values);
-		std_bids_in_an_auction       = np.std(auctions.values);
-		no_auctions_with_single_bid  = len(auctions.values[auctions.values==1]);
-		per_auctions_with_single_bid = no_auctions_with_single_bid / float(len(auctions.values));
+		no_of_distinct_auctions, min_bids_in_an_auction, \
+		max_bids_in_an_auction, avg_bids_in_an_auction, \
+		median_bids_in_an_auction, std_bids_in_an_auction, \       
+		no_auctions_with_single_bid, per_auctions_with_single_bid \
+		= getStats(auctions);
+
+
+		#Device Features
+		no_of_distinct_devices, min_bids_from_device, \
+		max_bids_from_device, avg_bids_from_device, \
+		median_bids_from_device, std_bids_from_device, \       
+		no_devices_with_single_bid, per_devices_with_single_bid \
+		= getStats(devices);
+
+
+		#Merchandize Features
+		no_of_distinct_merchandize, min_bids_for_merchandize, \
+		max_bids_for_merchandize, avg_bids_for_merchandize, \
+		med_bids_for_merchandize, std_bids_for_merchandize, \       
+		no_merch_with_single_bid, per_merch_with_single_bid \
+		= getStats(merchandise);
 
 
 
+		#Countries Features
+		no_of_distinct_countries, min_bids_from_countries, \
+		max_bids_from_countries, avg_bids_from_countries, \
+		med_bids_from_countries, std_bids_from_countries, \       
+		no_countries_with_single_bid, per_countries_with_single_bid \
+		= getStats(countries);
 
-		device      = len(group['device'].value_counts().keys());
-		merchandise = len(group['merchandise'].value_counts().keys());
-		country 	= len(group['country'].value_counts().keys());
-
-		print((country, auction, device, merchandise))
-
-		break;
-
+		#IPS Features
+		no_of_distinct_ips, min_bids_from_ip, \
+		max_bids_from_ip, avg_bids_from_ip, \
+		med_bids_from_ip, std_bids_from_ip, \       
+		no_ip_with_single_bid, per_ip_with_single_bid \
+		= getStats(ips);
 
 
+		#URLS Features
+		no_of_distinct_ips, min_bids_from_ip, \
+		max_bids_from_ip, avg_bids_from_ip, \
+		med_bids_from_ip, std_bids_from_ip, \       
+		no_ip_with_single_bid, per_ip_with_single_bid \
+		= getStats(ips);
+
+
+		per_auction_devices_list = [len(d['device'].unique()) for a,d in auction_devices]
+		per_auction_country_list = [len(c['country'].unique()) for a,c in auction_countries]
+		per_auction_ip_list 	 = [len(i['ip'].unique()) for a,i in auction_ips]
+
+		#Features
+		min_diff_devices_used_in_auction = np.min(per_auction_devices_list);
+		max_diff_devices_used_in_auction = np.max(per_auction_devices_list);
+		med_diff_devices_used_in_auction = np.median(per_auction_devices_list);
+
+		#Features
+		min_diff_countries_used_in_auction = np.min(per_auction_country_list);
+		max_diff_countries_used_in_auction = np.max(per_auction_country_list);
+		med_diff_countries_used_in_auction = np.median(per_auction_country_list);
+
+		#Features
+		min_diff_ip_used_in_auction = np.min(per_auction_ip_list);
+		max_diff_ip_used_in_auction = np.max(per_auction_ip_list);
+		med_diff_ip_used_in_auction = np.median(per_auction_ip_list);
 
 
 
